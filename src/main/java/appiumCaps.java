b75cb84544f7f760581530d1a12fda5304a27f12
Jonathon Ramey <Jonathon.Ramey@prometheanworld.com>
2015-06-25 17:09:11 -0400

    First test waits forever to show POC

--- src/main/java/appiumCaps.java
+++ src/main/java/appiumCaps.java
@@ -4,15 +4,17 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import org.junit.rules.TestName;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -30,13 +32,15 @@ public class appiumCaps {
     public String getSessionId() {
         return sessionId;
     }
-    public @Rule
+
+    public
+    @Rule
     TestName name = new TestName();
 
 
     @Before
     public void setUp() throws MalformedURLException {
-        File appDir = new File("~/Yikyak/");
+        File appDir = new File("C:\\Users\\jonathon.ramey\\Documents\\Yikyak");
         File app = new File(appDir, "YikYak-2.7.3.apk");
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("app", app.getAbsolutePath());
@@ -45,15 +49,13 @@ public class appiumCaps {
         desiredCapabilities.setCapability("appiumVersion", "1.3.4");
         desiredCapabilities.setCapability("name", name.getMethodName());
 
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
-        driver = new AndroidDriver(url, desiredCapabilities);
-        sessionId = driver.getSessionId().toString();
         try {
 
             // Connect to the appium server on localhost
             this.driver = new AndroidDriver(
                     new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities) {
+
             };
         } catch (MalformedURLException e
                 ) {
@@ -61,22 +63,28 @@ public class appiumCaps {
         }
         //Timeout is set to 80 seconds before the test quits
         driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+        sessionId = driver.getSessionId().toString();
 
     }
+
+
+    @Test
+    public void switchToHotPosts() {
+        WebDriverWait wait = new WebDriverWait(driver, 80);
+        getHotButton().click();
+        //Will Wait forever to show POC
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("/uF013")));
+
+    }
+
+    WebElement getHotButton() {
+        return driver.findElementByName("Hot");
+    }
+
 
     @After
     public void tearDown() {
         System.out.println("Test that was just run: " + this.getSessionId());
         driver.quit();
-    }
-
-    @Test
-    public void switchToHotPosts()
-    {
-        getHotButton().click();
-
-    }
-    WebElement getHotButton() {
-        return driver.findElementByName("Hot");
     }
 }
